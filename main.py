4bef3920b0dda53a684b9892a67b2ca787a3e85e
santiagoruizp <42008840+santiagoruizp@users.noreply.github.com>
2024-06-05 18:02:44 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -2,11 +2,24 @@
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
 from routers import sensordata
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()  # Crear una instancia de la aplicación FastAPI
 
 app.title = "UdeA - IOT API"  # Establecer el título de la aplicación
 app.version = "1.0.0"  # Establecer la versión de la aplicación
+
+origins = [
+"*","http://localhost",
+]
+
+app.add_middleware(CORSMiddleware,
+                   allow_origins=origins,
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"],
+                   )
+
 
 @app.get("/", tags=["Home"])  # Definir una ruta para la página de inicio
 def home():
